4932e601953d42cd87203f01903276db5f72d65f
pe4enka <girfanov_kostia@mail.ru>
2023-10-28 02:43:49 +0500

    очередь?...

--- AlgorythmLab3/Program.cs
+++ AlgorythmLab3/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AlgorythmLab3.Stack_and_Queue;
+
 Console.WriteLine("Hello, World!");
+
+var queue = new MyQueue<object>();
+queue.Enqueue("string 1");
+queue.Enqueue("string 2");
+queue.Enqueue("string 3");
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(3);
+
+queue.Print();
+Console.WriteLine();
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Dequeue());
