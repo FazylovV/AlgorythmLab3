75938670d36193bd4526b00bd7565e80315fc910
alwk <Xcom.D@yandex.ru>
2023-11-01 21:14:48 +0500

    Add Lists Menu

---
+++ AlgorythmLab3/ConsoleHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorythmLab3
+{
+    public class ConsoleHelper
+    {
+        public static void ClearScreen()
+        {
+            for (int i = 0; i < Console.WindowHeight; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write(new string(' ', Console.WindowWidth));
+            }
+            Console.SetCursorPosition(0, 0);
+        }
+
+    }
+
+}
