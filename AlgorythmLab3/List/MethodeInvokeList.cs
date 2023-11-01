844b726d33f475283bc39596e50af56908664cbd
alwk <Xcom.D@yandex.ru>
2023-11-01 21:52:36 +0500

    Add Stack and queue menu

--- AlgorythmLab3/List/MethodeInvokeList.cs
+++ AlgorythmLab3/List/MethodeInvokeList.cs
@@ -33,10 +33,9 @@ namespace AlgorythmLab3.List
         public new void ToString()
         {
             if (list.Count != 0)
-                Console.WriteLine($"Ваш список выглядит как-то так: {list}");
+                Console.WriteLine($"Текущее состояние списка: {list}");
             else
-                Console.WriteLine("Вы не увидите здесь своего списка... Но это не мы виноваты!" +
-                    " Просто он неотразим (пустой, получается).");
+                Console.WriteLine("Список пуст");
             Console.ReadKey();
         }
 
@@ -48,11 +47,11 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите значение нового головастика списка: ");
+                Console.Write("Введите значение нового первого элемента: ");
                 if (!EnterOneData(out T newData)) continue;
 
                 list.AddHead(newData);
-                Console.WriteLine($"Лучше и быть не может: {list}. Вам же нравится, правда?");
+                Console.WriteLine($"Теперь список выглядит так: {list}.");
                 break;
             }
             Console.ReadKey();
@@ -66,11 +65,11 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите значение нового хвостика списка: ");
+                Console.Write("Введите значение нового последнего элемента:  ");
                 if (!EnterOneData(out T newData)) continue;
 
                 list.AddTail(newData);
-                Console.WriteLine($"А у нас пополнение! Только взгляните на эту счастливую семью: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}.");
                 break;
             }
             Console.ReadKey();
@@ -83,12 +82,12 @@ namespace AlgorythmLab3.List
         {
             if (list.Count <= 1)
             {
-                Console.WriteLine("Извините, а что тут, собственно, переворачивать? Мы жаждем больше элементов!");
+                Console.WriteLine("Список содержит меньше элементов, чем требуется для разворота");
             }
             else
             {
                 list.Reverse();
-                Console.WriteLine($"Красивое, не правда ли: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}.");
             }
             Console.ReadKey();
         }
@@ -100,12 +99,12 @@ namespace AlgorythmLab3.List
         {
             if (list.Count <= 1)
             {
-                Console.WriteLine("А переносить-то и нечего! Добавьте элементиков - получите результат.");
+                Console.WriteLine("Список содержит меньше элементов, чем требуется");
             }
             else
             {
                 list.HeadToTail();
-                Console.WriteLine($"Не знаем, зачем вам это, но держите: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}.");
             }
             Console.ReadKey();
         }
@@ -117,12 +116,12 @@ namespace AlgorythmLab3.List
         {
             if (list.Count <= 1)
             {
-                Console.WriteLine("Сейчас-сейчас... А, ой! Вот то что вы пытаетесь сделать, вот это и невозможно... Мало элементов помоту что!");
+                Console.WriteLine("Список содержит меньше элементов, чем требуется");
             }
             else
             {
                 list.TailToHead();
-                Console.WriteLine($"Вот ваш заказ, держите и радуйтесь: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}");
             }
             Console.ReadKey();
         }
@@ -134,7 +133,7 @@ namespace AlgorythmLab3.List
         {
             if (list.Count <= 1 || list.DistinctCount() == 1)
             {
-                Console.WriteLine("Мы не работаем со списками, где недостаточно уникальных элементов! Ну вот что, скажите, тут можно поменять?");
+                Console.WriteLine("Список содержит слишком мало уникальных элементов");
                 Console.ReadKey();
                 return;
             }
@@ -142,19 +141,19 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите два переменчивых значения через ПРОБЕЛЬЧИК.\n" +
+                Console.Write("Введите два значения через пробел.\n" +
                     "Если для значения необходимы пробелы, замените их на значок доллара $: ");
-                string errorMessage = "Мы слишком глупенькие, чтобы воспринимать то, что вы написали. Пожалуйста, следуйте условиям.";
+                string errorMessage = "Ввод не соотвествует требованиям.";
                 if (!EnterTwoData(errorMessage, out T? data1, out T? data2)) continue;
 
                 if (data1.Equals(data2))
                 {
-                    Console.WriteLine("Наташ, они одинаковые! Введите что-то нормальное, пожалуйста, иначе мы вас не отпустим.");
+                    Console.WriteLine("Введены одинаковые элементы.");
                     Console.ReadKey();
                     continue;
                 }
                 list.Swap(data1, data2);
-                Console.WriteLine($"Обмен совершён. Надеемся, это была выгодная сделка: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}");
                 break;
             }
             Console.ReadKey();
@@ -167,7 +166,7 @@ namespace AlgorythmLab3.List
         {
             if (list.Count == 0)
             {
-                Console.WriteLine("Список пустует... Как в нём можно найти хоть какой-то элемент?");
+                Console.WriteLine("Список пуст");
                 Console.ReadKey();
                 return;
             }
@@ -175,14 +174,14 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите два значения через ПРОБЕЛЬЧИК. Первое - значение вставляемого элемента,\n" +
+                Console.Write("Введите два значения через пробел. Первое - значение вставляемого элемента,\n" +
                     "второе - до которого совершается вставка.\n" +
                     "Если для значения необходимы пробелы, замените их на значок доллара $: ");
-                string errorMessage = "Что-то написано не так... Перечитайте условия работы данного метода.";
+                string errorMessage = "Ввод не соотвествует требованиям.";
                 if (!EnterTwoData(errorMessage, out T? insertData, out T? requiredData)) continue;
 
                 list.InsertBefore(insertData, requiredData);
-                Console.WriteLine($"Мы сделали всё, что могли... Но не то чтобы вы ожидали от нас чего-то большего: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}");
                 break;
             }
             Console.ReadKey();
@@ -195,13 +194,13 @@ namespace AlgorythmLab3.List
         {
             if (list.Count == 0)
             {
-                Console.WriteLine("Копия пустого списка, конечно, звучит заманчиво, но мы на это не поведёмся!");
+                Console.WriteLine("Список пуст");
                 return;
             }
             else
             {
                 list.InsertCopyAsTail();
-                Console.WriteLine($"Поздравляем, у вас близняшки: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}");
             }
             Console.ReadKey();
         }
@@ -213,13 +212,12 @@ namespace AlgorythmLab3.List
         {
             if (list.Count <= 1)
             {
-                Console.WriteLine("Прочитайте запрос внимательно. Там же прямо написано: \"Удалить все дубликаты\".\n" +
-                    "Ну какие дубликаты могут быть в вашем масеньком списке?");
+                Console.WriteLine("Список содержит слишком мало элементов");
             }
             else
             {
                 list.RemoveDuplicates();
-                Console.WriteLine($"Того, чего вы так боялись, больше нет. Взгляните: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}");
             }
             Console.ReadKey();
         }
@@ -231,7 +229,7 @@ namespace AlgorythmLab3.List
         {
             if (list.Count == 0)
             {
-                Console.WriteLine("Ну допустим мы можем удалить воздух из этого списка, но элементов-то нет, удалять-то больше нечего.");
+                Console.WriteLine("Список пуст.");
                 Console.ReadKey();
                 return;
             }
@@ -239,11 +237,11 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите значение удаляемого элементика списка: ");
+                Console.Write("Введите значение удаляемого элемента списка: ");
                 if (!EnterOneData(out T removeData)) continue;
 
                 list.Remove(removeData);
-                Console.WriteLine($"Вы видите это страшненькое значение в списке: {list}? Вот и мы не видим. И его нет.");
+                Console.WriteLine($"Теперь список выглядит так: {list}.");
                 break;
             }
             Console.ReadKey();
@@ -256,13 +254,13 @@ namespace AlgorythmLab3.List
         {
             if (!TypeChecker.IsIntegerType(typeof(T)))
             {
-                Console.WriteLine("Этому методу нужен новый герой! То есть, другие типы данных списка.");
+                Console.WriteLine("Неподходящий тип данных.");
                 Console.ReadKey();
                 return;
             }
             else if (list.Count == 0)
             {
-                Console.WriteLine("Какое бы значение вы не указали, всё будет тщетно... Вам когда-нибудь говорили про то, что пустой список пуст?");
+                Console.WriteLine("Список пуст.");
                 Console.ReadKey();
                 return;
             }
@@ -270,12 +268,11 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите значение-указатель (Вы же в курсе, что оно должно быть в списке?): ");
+                Console.Write("Введите значение-указатель (Оно должно быть в списке): ");
                 if (!EnterOneData(out T newData)) continue;
 
                 list.InsertCopyAfter(newData);
-                Console.WriteLine($"Вот такая магия произошла: {list}.\n" +
-                    $"Говорят, если не знать, что делает этот метод, можно в попытках разгадать его тайну сломать мозг.");
+                Console.WriteLine($"Теперь список выглядит так: {list}.\n");
                 break;
             }
             Console.ReadKey();
@@ -288,19 +285,19 @@ namespace AlgorythmLab3.List
         {
             if (!TypeChecker.IsNumericType(typeof(T)))
             {
-                Console.WriteLine("Извините, мы не знаем, как это всё сравнивать... Укажите целочисленный тип данных для своего списка.");
+                Console.WriteLine("Неверный тип данных.");
                 Console.ReadKey();
                 return;
             }
             else if (!list.IsSortedInAcsendingOrder())
             {
-                Console.WriteLine("Перечитываем название метода... Ага-а... А вы уверены, что ваш список отсортирован по неубыванию?");
+                Console.WriteLine("Отсутствует сортировка");
                 Console.ReadKey();
                 return;
             }
             else if (list.Count == 0)
             {
-                Console.WriteLine("Списочек пустует, а этого быть не должно! Добавьте хотя бы один элементик.");
+                Console.WriteLine("Список пуст.");
                 Console.ReadKey();
                 return;
             }
@@ -308,11 +305,11 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите значение для его добавления в какое-то местечко среди элементов: ");
+                Console.Write("Введите значение для добавления: ");
                 if (!EnterOneData(out T newData)) continue;
 
                 list.InsertInSortedInAcsOrderList(newData);
-                Console.WriteLine($"Новый элементик уже в строю: {list}. Уверены, он справится со своей службой!");
+                Console.WriteLine($"Теперь список выглядит так: {list}.");
                 break;
             }
             Console.ReadKey();
@@ -325,7 +322,7 @@ namespace AlgorythmLab3.List
         {
             if (!TypeChecker.IsIntegerType(typeof(T)))
             {
-                Console.WriteLine("Капс был единственным нашим оружием... И оно не помогло...");
+                Console.WriteLine("Неверный тип данных");
                 Console.ReadKey();
                 return;
             }
@@ -333,12 +330,12 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите значения добавляемого в качестве хвостика списка через ПРОБЕЛЬЧИК.\n" +
+                Console.Write("Введите значения добавляемого в конец списка через пробел.\n" +
                     "Если для значения необходимы пробелы, замените их на значок доллара $: ");
                 if (!EnterListData(out LinkedList<T>? newList)) continue;
 
                 list.InsertAsTail(newList);
-                Console.WriteLine($"У вашего списочка вырос хвостик в соответствиями с вашими ожиданиями: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}");
                 break;
             }
             Console.ReadKey();
@@ -351,16 +348,15 @@ namespace AlgorythmLab3.List
         {
             if (!TypeChecker.IsIntegerType(typeof(T)))
             {
-                Console.WriteLine("Принимаем только целочисленные списки для этого метода!\n" +
-                    "Поменяйте списочек на тот, что с другим типом, если хотите увидеть что-то интересненькое.");
+                Console.WriteLine("Неверный тип данных.");
             }
             else if (list.Count == 0)
             {
-                Console.WriteLine("Этот список был слишком пустым внутри... Может, сам по себе он и уникален, но только не его элементы.");
+                Console.WriteLine("Список пуст.");
             }
             else
             {
-                Console.WriteLine($"Интересный факт, который вам нужно знать: уникального в вашем списке ровно {list.DistinctCount()}");
+                Console.WriteLine($"Количество уникальных элементов в списке: {list.DistinctCount()}");
             }
             Console.ReadKey();
         }
@@ -372,13 +368,13 @@ namespace AlgorythmLab3.List
         {
             if (!TypeChecker.IsIntegerType(typeof(T)))
             {
-                Console.WriteLine("ДЛЯ ЦЕЛЫХ ЧИСЕЛ! Да-да, там было написано именно так.");
+                Console.WriteLine("Неверный тип данных.");
                 Console.ReadKey();
                 return;
             }
             else if (list.Count == 0)
             {
-                Console.WriteLine("А вы знали, что в пустом списке нечего делить?");
+                Console.WriteLine("Список пуст.");
                 Console.ReadKey();
                 return;
             }
@@ -386,7 +382,7 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.Write("Введите значение элемента, который мощно разорвёт этот список на две части: ");
+                Console.Write("Введите значение элемента, по которому будет произведено разделение: ");
                 if (!EnterOneData(out T splitData)) continue;
 
                 LinkedList<T>? leftList = list;
@@ -414,8 +410,7 @@ namespace AlgorythmLab3.List
             }
             catch (ArgumentException)
             {
-                Console.WriteLine("Упс... Введённое значение не соответствует типу элементов списка.\n" +
-                    "Попробуйте снова, на этот раз грехом будет ошибиться!");
+                Console.WriteLine("Неверный тип данных");
                 Console.ReadKey();
                 result = default;
                 return false;
@@ -519,8 +514,8 @@ namespace AlgorythmLab3.List
             while (true)
             {
                 ConsoleHelper.ClearScreen();
-                Console.WriteLine($"Итак, теперь у нас есть два списочка. Первый би лайк: {firstList}. Второй: {secondList}\n" +
-                    $"Выбирайте списочек, с которым хотите работать нажатием на циферку 1 или 2 соответственно.");
+                Console.WriteLine($"Теперь существует два списка. Первый: {firstList}. Второй: {secondList}\n" +
+                    $"Выбирите список, с которым хотите работать нажатием на цифру 1 или 2 соответственно.");
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
 
                 if (keyInfo.Key == ConsoleKey.NumPad1 || keyInfo.Key == ConsoleKey.D1)
@@ -533,13 +528,13 @@ namespace AlgorythmLab3.List
                 }
                 else
                 {
-                    Console.WriteLine("Кажется, вы нажали не ту кнопочку... Попробуйте ещё раз");
+                    Console.WriteLine("Нажата не та клавиша");
                     Console.ReadKey();
                     continue;
                 }
 
                 ConsoleHelper.ClearScreen();
-                Console.WriteLine($"Вам удалось нажать верную кнопку! А потому ваш текущий список теперь выглядит вот так: {list}");
+                Console.WriteLine($"Теперь список выглядит так: {list}");
                 break;
             }
             Console.ReadKey();
